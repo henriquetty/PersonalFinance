71b401b46f2830c3a4ddfdb4cddf907e30d31f52
gameramante <gamercaipira@gmail.com>
2019-10-08 17:30:33 -0300

    logic to persist in local storage added

--- javascript/index.js
+++ javascript/index.js
@@ -1,48 +1,61 @@
-window.addEventListener('DOMContentLoaded', function() {
+//index
 
+let btnIndex = window.document.querySelector('#buttonIndex button').addEventListener('click', createFinance);
+
+class Finance {
+    constructor(year, month, day, activity, description, totSpent){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.activity = activity;
+        this.description = description;
+        this.totSpent = totSpent;
+    }
+}
+
+class PersistLocalStorage {
+
+    constructor(){
+        let id = localStorage.getItem('id');
+        if (id === null){
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getNextId(){
+        let nextId = localStorage.getItem('id');
+        return Number.parseInt(nextId) + 1;
+    }
+
+    persist(items){
+        this.id = this.getNextId();
+        localStorage.setItem(this.id, JSON.stringify(items));
+        localStorage.setItem('id', this.id);
+    }
+}
+
+let persistLocalStorage = new PersistLocalStorage();
+
+function createFinance(){
     let inputs = {
-        year: document.querySelector('.year select'),
-        month: document.querySelector('.month select'),
-        activity: document.querySelector('.activity select'),
-        day: document.querySelector('.day input'),
-        description: document.querySelector('.description input'),
-        price: document.querySelector('.price input')
+        year: window.document.querySelector('#main-area .year select').value,
+        month: window.document.querySelector('#main-area .month select').value,
+        day: window.document.querySelector('#main-area .day input').value,
+        activity: window.document.querySelector('#main-area .activity select').value,
+        description: window.document.querySelector('#main-area .description input').value,
+        totSpent: window.document.querySelector('#main-area .price input').value
     };
 
-    let activities = [];
+    let = { year, month, day, activity, description, totSpent } = inputs;
 
-    const generateActivity = (values) => {
-        let container = document.createElement('div');
+    let finance = new Finance( //parameters to class Finance
+        year,
+        month,
+        day,
+        activity,
+        description,
+        totSpent
+    );
 
-        let activity = document.createElement('p');
-        activity.innerText = values.activity || 'No activity.';
-
-        let description = document.createElement('p');
-        description.innerText = values.description || 'No description.';
-
-        let price = document.createElement('span');
-        price.innerText = values.price || 'No price.';
-
-        let timestamp = document.createElement('span');
-        timestamp.innerText = `${values.day || 1}/${values.month || 1}/${values.year || 1900}`
-
-        container.append(timestamp);
-        container.append(activity);
-        container.append(description);
-        container.append(price);
-
-        document.querySelector('.activities').append(container);
-
-        activities.push(values)
-    }
-    
-
-    document.querySelector('#buttonIndex button').addEventListener('click', () => {
-        let input_values = {};
-
-        Object.keys(inputs).map(e => input_values[e] = inputs[e].value );
-
-        generateActivity(input_values);
-    })
-
-});
+    persistLocalStorage.persist(finance); //parameters to class PersistLocalStorage
+}
